dbc9c5ddddf24a6a8d1db93e02cf0e0f53e76bd1
HimanshuBohra <himanshupbohra@gmail.com>
2024-03-11 23:27:17 +0530

    serup react and node and made connection

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
+app.use(cors())
 
 app.get('/',(request,response) => {
-    response.send("Response");
+
+    const data = {};
+    data.name = "Himanshu";
+    data.age = 26;
+    response.send(data);
 })
     
 app.listen(8080, () =>{
